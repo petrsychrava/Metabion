1e62d123a108922e43b785726e33d5149475ce2d
petrsychrava <petrsychrava@gmail.com>
2026-07-06 13:09:21 +0200

    Add MCP OAuth authorization properties

--- src/main/java/com/metabion/Main.java
+++ src/main/java/com/metabion/Main.java
@@ -1,12 +1,13 @@
 package com.metabion;
 
+import com.metabion.config.OAuthAuthorizationProperties;
 import com.metabion.service.DietLogPhotoProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableConfigurationProperties(DietLogPhotoProperties.class)
+@EnableConfigurationProperties({DietLogPhotoProperties.class, OAuthAuthorizationProperties.class})
 @EnableScheduling
 @SpringBootApplication
 public class Main {
